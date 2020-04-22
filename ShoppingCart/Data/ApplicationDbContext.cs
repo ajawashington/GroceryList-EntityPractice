4ae393c146c078cb59dcd92d123975324023479e
Rose Wisotzky <rose@rosewisotzky.com>
2020-04-22 11:34:47 -0500

    seeding the database

--- ShoppingCart/Data/ApplicationDbContext.cs
+++ ShoppingCart/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ShoppingCart.Models;
@@ -16,5 +17,59 @@ namespace ShoppingCart.Data
 
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
         public DbSet<ShoppingItem> ShoppingItem { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Create a new user for Identity Framework
+            ApplicationUser user = new ApplicationUser
+            {
+                FirstName = "Rose",
+                LastName = "Wiz",
+                UserName = "r@r.com",
+                NormalizedUserName = "R@R.COM",
+                Email = "r@r.com",
+                NormalizedEmail = "R@R.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = "7f434309-a4d9-48e9-9ebb-8803db794577",
+                Id = "00000000-ffff-ffff-ffff-ffffffffffff"
+            };
+
+            var passwordHash = new PasswordHasher<ApplicationUser>();
+            user.PasswordHash = passwordHash.HashPassword(user, "Admin8*");
+            modelBuilder.Entity<ApplicationUser>().HasData(user);
+
+            //Create shopping items 
+            ShoppingItem item = new ShoppingItem
+            {
+                Id = 1,
+                ProductName = "Can of Spam",
+                ApplicationUserId = "00000000-ffff-ffff-ffff-ffffffffffff"
+            };
+            modelBuilder.Entity<ShoppingItem>().HasData(item);
+
+            modelBuilder.Entity<ShoppingItem>().HasData(
+                new ShoppingItem()
+                {
+                    Id = 2,
+                    ProductName = "Rice",
+                    ApplicationUserId = "00000000-ffff-ffff-ffff-ffffffffffff"
+                },
+                new ShoppingItem()
+                 {
+                     Id = 3,
+                     ProductName = "Eggs",
+                     ApplicationUserId = "00000000-ffff-ffff-ffff-ffffffffffff"
+                 },
+                 new ShoppingItem()
+                  {
+                      Id = 4,
+                      ProductName = "Mask",
+                      ApplicationUserId = "00000000-ffff-ffff-ffff-ffffffffffff"
+                  }
+
+                 );
+        }
     }
 }
---
+++ ShoppingCart/Migrations/20200422162345_Seeding.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShoppingCart.Migrations
+{
+    public partial class Seeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "00000000-ffff-ffff-ffff-ffffffffffff", 0, "7a431db7-3f75-4076-8472-a61c6012ace2", "r@r.com", true, "Rose", "Wiz", false, null, "R@R.COM", "R@R.COM", "AQAAAAEAACcQAAAAEIhKOMYUZUEMBwuBBsFaHzm9svv86KShWvLFsmZrTKiB9oMt3m9Onky1ROEp2ek2og==", null, false, "7f434309-a4d9-48e9-9ebb-8803db794577", false, "r@r.com" });
+
+            migrationBuilder.InsertData(
+                table: "ShoppingItem",
+                columns: new[] { "Id", "ApplicationUserId", "ProductName" },
+                values: new object[,]
+                {
+                    { 1, "00000000-ffff-ffff-ffff-ffffffffffff", "Can of Spam" },
+                    { 2, "00000000-ffff-ffff-ffff-ffffffffffff", "Rice" },
+                    { 3, "00000000-ffff-ffff-ffff-ffffffffffff", "Eggs" },
+                    { 4, "00000000-ffff-ffff-ffff-ffffffffffff", "Mask" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ShoppingItem",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ShoppingItem",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ShoppingItem",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ShoppingItem",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "00000000-ffff-ffff-ffff-ffffffffffff");
+        }
+    }
+}
